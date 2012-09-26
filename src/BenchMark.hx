300fd2b4768f62e0125e96a1403faa40603f2da5
fukaoi <fukaoi@debian.fukaoi.org>
2012-09-26 15:21:23 +0900

    added start, end method

--- src/BenchMark.hx
+++ src/BenchMark.hx
@@ -13,57 +13,86 @@ class BenchMark {
   private var args(default, null):Array<Dynamic>;
   private var pos(default, null):PosInfos;
 
-  /*
-   * func:Target function or method
-   * obj:Setting this if func is instance method
-   * args:for func of parameter
-   * pos: In using, no need
-   *
-   * Usage:
-   * var bench = new BenchMark(Test, check, []);
-   * bench.start(10);
-   * bench.end();
-   *
-   * Result:
-   * [target name] Test.check
-   * [loop count] 10
-   * [time] 0.011/sec
-   *
-   */
-  public function new(func:Dynamic,
-                      ?obj:Dynamic = null,
-                      ?args:Array<Dynamic> = null,
-                      ?pos:PosInfos) {
-    this.func = func;
-    this.obj = obj;
-    this.args = args;
-    this.pos = pos;
-  }
-  /*
-   * Bench start method.before end() execute
-   * loopCount: number of times
-   */
-  public function start(loopCount:Int = 1) {
+  /**
+    Constructor
+
+    func:Target function or method
+    obj:Setting this if func is instance method
+    args:for func of parameter
+    pos: In using, no need
+
+    Usage:
+    var bench = new BenchMark(Test, check, []);
+    bench.start(10);
+    bench.end();
+
+    Result:
+    [target name] Test.check
+    [loop count] 10
+    [time] 0.011/sec
+
+  **/
+  public function new(loopCount:Int = 1) {
     this.loopCount = loopCount;
-    for (i in 0...loopCount) {
-      startTime = Date.now().getTime();
-      Reflect.callMethod(obj, func, args);
-    }
   }
-  /*
-   * Bench end method.
-   */
-  public function end() {
+
+  /**
+    set starting microtime
+  **/
+  public function start(?pos:PosInfos):Void {
+    this.pos = pos;
+    trace("\n---------------------------");
+    startTime = Date.now().getTime();
+  }
+
+  /**
+    get result time and display
+  **/
+  public function end():Void {
     endTime = Date.now().getTime();
     var resultTime = endTime - startTime;
     disp(resultTime / 1000);
   }
-  /*
-   * Display of results
-   */
+
+  /**
+    Bench start method.before end() execute
+    loopCount: number of times
+  **/
+  public function startExecFunc(func:Dynamic,
+                                ?obj:Dynamic = null,
+                                ?args:Array<Dynamic> = null,
+                                ?pos:PosInfos):Array<Dynamic> {
+    this.func = func;
+    this.obj = obj;
+    this.args = args;
+    this.pos = pos;
+
+    var results = [];
+    trace("\n---------------------------");
+    startTime = Date.now().getTime();
+    for (i in 0...loopCount) {
+      results.push(Reflect.callMethod(obj, func, args));
+    }
+    return results;
+  }
+
+  /**
+    Bench end method.
+  **/
+  public function endExecFunc():Void {
+    endTime = Date.now().getTime();
+    var resultTime = endTime - startTime;
+    disp(resultTime / 1000);
+  }
+
+  /**
+    Display of results
+  **/
   private function disp(resultTime:Float) {
     var funcName = pos.className + "." + pos.methodName;
+    var lineNumber = pos.lineNumber;
     trace("[target name] $funcName".format());
+    trace("[line number] $lineNumber".format());
     trace("[loop count] $loopCount".format());
     trace("[time] $resultTime/sec".format());
   }
